b9af966488013ed8e235b96194229fa23af0f6a8
clarovideo <155674629+FutbolCrackshn@users.noreply.github.com>
2024-02-03 01:08:15 -0600

    deportivo.js

---
+++ deportivo.js
@@ -0,0 +1,37 @@
+// JScript File
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    MacOSX: function() {
+        return navigator.userAgent.match(/Macintosh/i);
+    },	
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+var embedded = "mobile";
+if (!isMobile.any()){
+        embedded = "desktop";
+}
+
+if(v_height == "100%"){
+    PlaySize = 'style="overflow:hidden;height:100%;width:100%"';
+}
+else{
+    PlaySize = '';
+}
+document.write('<ifr'+'ame src="https://lovesomecommunity.com/deportivo.php?player='+ embedded +'&live='+ fid +'" '+PlaySize +' width='+ v_width +' height=' + v_height + ' scrolling="no" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></ifr'+'ame>') ;
+/**/
